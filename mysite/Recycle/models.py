6f0c91e1b31e77bc29ddcaabcb5f73dc1741fb04
JavierGanaa <143565756+JavierGanaa@users.noreply.github.com>
2023-12-03 18:05:50 -0300

    Add files via upload

--- mysite/Recycle/models.py
+++ mysite/Recycle/models.py
@@ -6,6 +6,8 @@ class Usuario(models.Model):
     nombre = models.CharField(max_length=20)
     correo = models.CharField(max_length=30, unique=True)
     contrasena = models.CharField(max_length=30)
+    id_evento= models.IntegerField(default=0)
+
 
     def __str__(self):
         return self.nombre 
