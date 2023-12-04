7777523f47ed0df2947d0a69658f8cdb08391354
JavierGanaa <143565756+JavierGanaa@users.noreply.github.com>
2023-12-04 15:46:30 -0300

    Add files via upload

--- mysite/Recycle/forms.py
+++ mysite/Recycle/forms.py
@@ -2,10 +2,34 @@ from django import forms
 from .models import Usuario
 
 class UsuarioForm(forms.ModelForm):
+    nombre = forms.CharField(max_length=30, required=False)
+    username = forms.CharField(max_length=30, required=True)
+    password = forms.CharField(max_length=30, strip=False, widget=forms.PasswordInput, required=True)
+    conf_contrasena = forms.CharField(max_length=30, widget=forms.PasswordInput(), label='Confirmar contraseña', required=False)
+    
     class Meta:
         model = Usuario
         fields = [
             'nombre',
-            'correo',
-            'contrasena',
+            'username',  #correo (debe llamarse username para iniciar sesion correctamente)
+            'password',
+            'conf_contrasena',
         ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        nombre = cleaned_data.get("nombre")
+        password = cleaned_data.get("password")
+        conf_contrasena = cleaned_data.get("conf_contrasena")
+        
+        #if nombre=="":
+            #conf_contrasena = cleaned_data.get("password")
+        if password != conf_contrasena:
+            raise forms.ValidationError("Las contraseñas no coinciden, vuelve a intentar.")  
+        
+ 
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(max_length=30)
+    password = forms.CharField(max_length=30, widget=forms.PasswordInput)        
+            
--- mysite/Recycle/models.py
+++ mysite/Recycle/models.py
@@ -1,13 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class Usuario(models.Model):
-    nombre = models.CharField(max_length=20)
-    correo = models.CharField(max_length=30, unique=True)
-    contrasena = models.CharField(max_length=30)
-    id_evento= models.IntegerField(default=0)
 
+class Usuario(AbstractUser):
+    nombre = models.CharField(max_length=20)
+    username = models.CharField(max_length=30, unique=True)
+    password = models.CharField(max_length=30)
+    id_evento = models.CharField(max_length=50, default="a")
+    #last_login = models.DateTimeField(null=True, blank=True)
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        related_name='custom_user_groups',  # Cambia 'custom_user_groups' según tus necesidades
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name='custom_user_permissions',  # Cambia 'custom_user_permissions' según tus necesidades
+    )
 
     def __str__(self):
-        return self.nombre 
+        return self.nombre
